cd0a278c2d6d296cb419bf866856218103b6ea0c
shashank <shashankskb18700@gmail.com>
2021-08-09 02:22:29 +0530

    add input to params

--- src/AnimeInfo.js
+++ src/AnimeInfo.js
@@ -8,39 +8,67 @@ class AnimeInfo extends React.Component {
     super(props);
     this.state = {
       data: [],
-      term: ""
+      term: "",
+      url: ""
       
     }
     
   }
   
-  async componentDidUpdate() {
-   
-    const apiData = await axios.get(`https://api.jikan.moe/v3/search/anime?q=${this.state.term}`);
-    console.log(apiData.data.results)
-    this.setState({ data: apiData.data });
-    
-  }
-  setTerm = (e) => {
+  componentDidMount() {
+     
+    const data = async() => {
+      const apiData = await axios.get(`https://api.jikan.moe/v3/search/anime?q=${this.state.term}`);
+      console.log(apiData.config.url)
       
-    this.setState({term:e.target.value})
+        
+        this.setState({ data: apiData.data }); 
+      
+    }
+    
+    
+    if (this.state.term === 0) {
+      data();
+    } else {
+      const timeoutId = setTimeout(() => {
+        if (this.state) {
+          data();
+        }
+      }, 2000);
+
+      return () => {
+        clearTimeout(timeoutId);
+      };
+    } 
+  }
+
+  setTerm = (e) => {
+   
+    
+     this.setState({ term: e.target.value });
+    // const timeStamp = setTimeout((e)=>{
+      
+    //   this.setState({term:e.target.value})
+    // }, 2000)
+    // return () => {
+    //   clearTimeout(timeStamp);
+    // }
     
   }
 
 
   render() {
-    console.log(this.state.data)
+    console.log(this.state.term)
     return (
-      <div>
-        <form onSubmit={this.setTerm} className='inpu'>
-          <input type="text"
-            
+      <div className="inpu">
+          
+          <input
+            type="text"
             placeholder="search"
             value={this.state.term}
-            onChange={this.setTerm} 
-            
-            />
-        </form>
+            onChange={this.setTerm}
+          />
+       
 
         <AnimeRender detail={this.state} />
         {console.log(this.state)}
--- src/AnimeRender.js
+++ src/AnimeRender.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 const AnimeRender = props => {
-  console.log(props.detail.results);
+  console.log(props);
 
   // the problem is props.detials.results is render before the a request compelted by api  calls 
    
 
     
-  const da = props.detail.results != undefined ? props.detail.results.map((url) => {
+  const da = props.detail.results !== undefined ? props.detail.results.map((url) => {
     console.log(url.url);
     return (<div>
       <img src={url.image_url} />
